b2f1c1ac19ec6433cc1e584da696b3ebc2acbd00
vandung93 <dungnguyenvanitnews@gmail.com>
2022-10-11 18:40:54 +0700

    Updated

--- src/dotinvietnam/assets/js-code/frontend.js
+++ src/dotinvietnam/assets/js-code/frontend.js
@@ -11,6 +11,45 @@
 			scrollTo();
 			initVideoPopups();
 			initSliders();
+
+			var cursor = $( "#custom-cursor" );
+
+			$( window ).mousemove( function( e ) {
+				cursor.css( {
+					top: e.clientY - cursor.height() / 2,
+					left: e.clientX - cursor.width() / 2
+				} );
+			} );
+
+			$( window ).mouseleave( function() {
+				cursor.css( {
+					opacity: "0"
+				} );
+			} ).mouseenter( function() {
+				cursor.css( {
+					opacity: "1"
+				} );
+			} );
+
+			$( "a" ).mouseenter( function() {
+				cursor.css( {
+					transform: "scale(2)"
+				} );
+			} ).mouseleave( function() {
+				cursor.css( {
+					transform: "scale(1)"
+				} );
+			} );
+
+			$( window ).mousedown( function() {
+				cursor.css( {
+					transform: "scale(.2)"
+				} );
+			} ).mouseup( function() {
+				cursor.css( {
+					transform: "scale(1)"
+				} );
+			} );
 		} );
 
 		$window.on( 'load', function() {
